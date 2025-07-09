581e2a3d64e5d3905d2802da1f42b66318cec895
congnam101 <22dh112252@st.huflit.edu.vn>
2025-07-09 13:29:36 +0700

    Update dp.php

--- app/dp.php
+++ app/dp.php
@@ -1,12 +1,18 @@
 <?php
 $host = 'db';
-$dbname = 'web';
+$db   = 'web';
 $user = 'root';
 $pass = 'root';
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+];
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
-} catch (PDOException $e) {
-    die("Lỗi kết nối DB: " . $e->getMessage());
+    $pdo = new PDO($dsn, $user, $pass, $options);
+} catch (\PDOException $e) {
+    throw new \PDOException($e->getMessage(), (int)$e->getCode());
 }
-?>
