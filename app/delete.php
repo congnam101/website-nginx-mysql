531bebaf044afd7ff767c6170e4ad18a3e5fd18a
congnam101 <22dh112252@st.huflit.edu.vn>
2025-07-09 14:03:22 +0700

    Update delete.php

--- app/delete.php
+++ app/delete.php
@@ -1,10 +1,9 @@
 <?php
 require 'db.php';
 
-$id = $_GET['id'] ?? 0;
-if ($id) {
-    $stmt = $pdo->prepare("DELETE FROM products WHERE id = ?");
-    $stmt->execute([$id]);
-}
+$id = $_GET['id'];
+$stmt = $pdo->prepare("DELETE FROM products WHERE id = ?");
+$stmt->execute([$id]);
 
-header("Location: index.php");
+header('Location: index.php');
+exit;
