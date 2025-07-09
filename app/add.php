8ba3958531761def3e839779f6460a8315bff528
congnam101 <22dh112252@st.huflit.edu.vn>
2025-07-09 14:22:17 +0700

    Update add.php

--- app/add.php
+++ app/add.php
@@ -4,8 +4,10 @@ require 'db.php';
 $name = $_POST['name'];
 $price = $_POST['price'];
 $quantity = $_POST['quantity'];
+$stock = $_POST['stock'];
 
-$stmt = $pdo->prepare("INSERT INTO products (name, price, quantity) VALUES (?, ?, ?)");
-$stmt->execute([$name, $price, $quantity]);
+$sql = "INSERT INTO products (name, price, quantity, stock) VALUES (?, ?, ?, ?)";
+$stmt = $pdo->prepare($sql);
+$stmt->execute([$name, $price, $quantity, $stock]);
 
-header("Location: index.php");
+header('Location: index.php');
