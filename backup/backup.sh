5c9fc3c4eff3d0cd9fbceb1f06541b98e33c74f8
congnam101 <22dh112252@st.huflit.edu.vn>
2025-07-11 13:57:38 +0700

    Update backup.sh

--- backup/backup.sh
+++ backup/backup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 TIMESTAMP=$(date +"%F-%H-%M")
-BACKUP_DIR="/backup/$TIMESTAMP"
+BACKUP_DIR="/home/congnam/Downloads/website-nginx-mysql/backup/$TIMESTAMP"
 mkdir -p "$BACKUP_DIR"
 docker exec mysql_db sh -c 'exec mysqldump -uroot -proot mydb' > "$BACKUP_DIR/db.sql"
